7a56195b12d75a4efc5f482980ba9b82ed91803b
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-04-17 18:27:15 +0500

    listing

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Listing from './components/Listing';
+import data from './data/etsy.json';
+
+function App() {
+    return <Listing items={data} />;
+}
+
+export default App;
